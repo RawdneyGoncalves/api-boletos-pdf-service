21d27b7e708f1d2a20b576e6c1f69fe4ef200303
Rawdney <mendesrawdney@gmail.com>
2025-04-12 11:49:08 -0300

    feat(di): configure InversifyJS dependency injection container
    - Set up singleton scoped bindings for repositories
    - Bind services and controllers using interface contracts
    - Configure container with auto-binding and default singleton scope
    - Ensure type-safe dependency resolution

--- src/container/inversify.ts
+++ src/container/inversify.ts
@@ -1,25 +1,61 @@
 import { Container } from 'inversify';
 import 'reflect-metadata';
-import { TYPES } from '../types/types';
+import { TYPES } from '../types/types.js';
 
 import { LotRepository } from '../repository/LotRepository.js';
 import { BillRepository } from '../repository/BillRepository.js';
 
-import { BillService } from '../services/BillService';
-import { CSVService } from '../services/CSVService';
-import { PDFService } from '../services/PDFService';
+import { LotService } from '../services/LotService.js';
+import { BillService } from '../services/BillService.js';
+import { CSVService } from '../services/CSVService.js';
+import { PDFService } from '../services/PDFService.js';
 
+import { LotController } from '../controller/LotController.js';
 import { BillController } from '../controller/BillController.js';
 
-const container = new Container();
+import { 
+  ILotRepository, 
+  IBillRepository, 
+  ILotService, 
+  IBillService, 
+  ICSVService, 
+  IPDFService 
+} from '../types/types.js';
 
-container.bind(TYPES.Repositories.LotRepository).to(LotRepository).inSingletonScope();
-container.bind(TYPES.Repositories.BillRepository).to(BillRepository).inSingletonScope();
+const container = new Container({
+  defaultScope: 'Singleton'
+});
 
-container.bind(TYPES.Services.BillService).to(BillService).inSingletonScope();
-container.bind(TYPES.Services.CSVService).to(CSVService).inSingletonScope();
-container.bind(TYPES.Services.PDFService).to(PDFService).inSingletonScope();
+container.bind<ILotRepository>(TYPES.Repositories.LotRepository)
+  .to(LotRepository)
+  .inSingletonScope();
 
-container.bind(TYPES.Controllers.BillController).to(BillController).inSingletonScope();
+container.bind<IBillRepository>(TYPES.Repositories.BillRepository)
+  .to(BillRepository)
+  .inSingletonScope();
+
+container.bind<ILotService>(TYPES.Services.LotService)
+  .to(LotService)
+  .inSingletonScope();
+
+container.bind<IBillService>(TYPES.Services.BillService)
+  .to(BillService)
+  .inSingletonScope();
+
+container.bind<ICSVService>(TYPES.Services.CSVService)
+  .to(CSVService)
+  .inSingletonScope();
+
+container.bind<IPDFService>(TYPES.Services.PDFService)
+  .to(PDFService)
+  .inSingletonScope();
+
+container.bind(TYPES.Controllers.LotController)
+  .to(LotController)
+  .inSingletonScope();
+
+container.bind(TYPES.Controllers.BillController)
+  .to(BillController)
+  .inSingletonScope();
 
 export { container };
